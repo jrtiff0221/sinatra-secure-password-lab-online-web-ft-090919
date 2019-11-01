c2fe1acac34b9a0e68e7047605e362a2a07de8f6
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-11-01 20:07:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
 
-    if user && user.save
+    if params[:username] && user && user.save
       redirect "/login"
     else
       redirect "/failure"
